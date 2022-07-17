a8ee4e99ea7c24017470db7f7751bde5a3762e26
dddwsd <quswotmd5@naver.com>
2022-07-17 22:54:00 +0900

    (Client - UI/UX) Implements initial client design

---
+++ short_sns/lib/search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'search',
+        style: TextStyle(fontSize: 30),
+      ),
+    );
+  }
+}
